dbbf6c7ba07d7340875dd29dc7bed35f4c9a42a9
Daniel Bonn <danieltbonn@gmail.com>
2023-12-15 22:06:13 -0600

    added a navbar and header with minimal css styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
+import './Header.css'
 import Navbar from "./Navbar";
 
 
 function Header() {
     return (
         <div className="header-main">
-            <p>Hello World!</p>
+            <Navbar />
         </div>
     )
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,20 @@
+import { Link } from 'react-router-dom';
+import {
+    Navbar,
+    Nav,
+    Container
+} from 'react-bootstrap';
 
-function Navbar() {
+function AppNavbar() {
+
+    return (
+        <Navbar expand="lg" className="navbar">
+            <Container fluid>
+                <Navbar.Brand as={Link} to="/" style={{fontSize: "34px", textDecoration: "none", color: "#000000"}}>REACT CHAT</Navbar.Brand>
+            </Container>
+        </Navbar>
+    )
 
 }
 
-export default Navbar;
+export default AppNavbar;
