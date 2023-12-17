509486c30e9c9e074c7fd3ea81a0daea7860a0a2
Daniel Bonn <danieltbonn@gmail.com>
2023-12-16 18:28:18 -0600

    added update_chat action to actions and a switch case to reducers

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,13 @@
+import { UPDATE_CHAT } from "./actions";
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case UPDATE_CHAT:
+            return {
+                ...state,
+                messages: [...action.messages]
+            };
+        default:
+            return state;
+    }
+}
