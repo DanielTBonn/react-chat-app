ca810ef0f038629dcb5b5b0a0b21f8ee0781baf4
Daniel Bonn <danieltbonn@gmail.com>
2023-12-16 18:38:25 -0600

    added functionality for message items to show up on the page

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -2,13 +2,19 @@ import './Chat.css'
 import {
     Container
 } from 'react-bootstrap'
+import { useStoreContext } from '../utils/GlobalState';
 
 function Chat() {
+
+    const [state] = useStoreContext(); 
+    console.log(state)
 
     return (
         <div>
             <Container className="container-chat" >
-                <p>Hello World!</p>
+                {state.messages ? state.messages.map((message) => (
+                    <p>{message}</p>
+                    )) : <p>Hello World!</p>}
             </Container>
         </div>
     )
