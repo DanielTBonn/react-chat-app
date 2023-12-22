de108ab1e93708455c93c8ad7d7b434eef72c687
Daniel Bonn <danieltbonn@gmail.com>
2023-12-21 20:51:26 -0600

    added url to website paths

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 const IS_PROD = process.env.NODE_ENV === "production";
 
-const SERVER_HOST = IS_PROD ? "website" : 'http://localhost:3001'
+const SERVER_HOST = IS_PROD ? "https://damp-woodland-03887-8c2ffd4fa0f9.herokuapp.com/" : 'http://localhost:3001'
 
 const socket = io(SERVER_HOST, {
     withCredentials: true,
