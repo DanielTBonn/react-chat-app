4927aa92b8722e561c4f2e9672295185ffd23db9
Daniel Bonn <danieltbonn@gmail.com>
2024-01-09 09:45:05 -0600

    added homepage to create chatrooms

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from './App';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from './pages/HomePage'
 
 const router = createBrowserRouter([
     {
@@ -8,7 +9,10 @@ const router = createBrowserRouter([
         element: <App />,
         errorElement: <h1 className="display-2">Wrong Page!</h1>,
         children: [
-
+            {
+                path: '/home',
+                element: <HomePage />
+            },
         ]
     }
 ])
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,42 @@
+import { 
+    Container,
+    Form,
+    Button 
+} from 'react-bootstrap';
 
 function HomePage() {
 
+    const [ chatRoom, setChatRoom ] = useState('')
+
+    const handleInputChange = (e) => {
+        e.preventDefault();
+
+        const { value } = e.target;
+        setChatRoom(value)
+    }
+
     return (
         <div>
-            <p>Hello HomePage</p>
+            <Container>
+                <Form>
+                    <Form.Group>
+
+                        <Form.Control>
+                            name="chat-room"
+                            value={chatRoom}
+                            onChange{(e) => handleInputChange(e)}
+                            type="text"
+                            placeholder="Create or Find a Chat Room..."
+                        </Form.Control>
+                    </Form.Group>
+                    <Button
+                        onClick={console.log('submitted')}
+                    >
+                        Send 
+                    </Button>
+                </Form>
+            </Container>
+            <p>Is this thing on?</p>
         </div>
     )
 }
